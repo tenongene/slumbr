2b7918cbcbee815ea939906c8209c1c14f15f1d4
Thierry Enongene <tenongene3@gatech.edu>
2025-03-22 23:34:31 -0500

    gauge added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
-import 'survey-core/survey-core.min.css';
-import { Model } from 'survey-core';
-import { Survey } from 'survey-react-ui';
-import { ContrastDark } from "survey-core/themes";
-import { ContrastLight } from "survey-core/themes";
 import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
+import { Card, Typography, Button } from "@material-tailwind/react";
 
-const surveyJson = {
-  elements: [{
-    name: "FirstName",
-    title: "Enter your first name:",
-    type: "text"
-  }, {
-    name: "LastName",
-    title: "Enter your last name:",
-    type: "text"
-  }]
-};
 
 function Home() {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const survey = new Model(surveyJson);
+  
 
   return (
     <div className="flex h-screen overflow-hidden">
@@ -35,9 +20,62 @@ function Home() {
 
        {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        
 
-        <Survey model={survey} />
+        <main className="grow">
 
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto" >
+          <Card className="flex h-full w-full max-w-[40rem] flex-row ml=15">
+              <Card.Header className="m-0 h-full w-2/5 shrink-0 rounded-r-none">
+                <img
+                  src="../src/images/awake.jpg"
+                  alt="card-image"
+                  className="h-full w-full object-cover"
+                />
+              </Card.Header>
+                  <Card.Body className="p-4">
+                     <Typography
+                  type="small"
+                  className="mb-4 font-bold uppercase text-foreground"
+                >
+                  Insomnia
+                </Typography>
+                <Typography type="h5" className="mb-2">
+      
+                </Typography>
+                <Typography className="mb-8 text-foreground">
+                Insomnia is a common sleep disorder characterized by difficulty falling asleep, 
+                staying asleep, or waking up too early, despite having adequate opportunity to sleep. 
+                It can be a temporary or chronic condition. 
+                <div className="mt-5">
+                  <strong>Slumbr</strong> can help you manage insomnia based on your lifestyle and other 
+                factors that influence your sleep quality. 
+                Please complete the assessment to analyze changes in your lifestyle and to receive a personalized recommendation on steps
+                to get you sleeping again!
+                </div>
+                </Typography>
+                <Button as="a" href="#" className="mb-2 pl-3 flex w-fit items-right gap-4">
+                  Begin Assessment
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    className="h-4 w-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                    />
+                  </svg>
+                </Button>
+              </Card.Body>
+            </Card>
+          </div> 
+
+        </main>
          
       </div>
 
--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -57,20 +57,18 @@ function Sidebar({
 
   return (
     <div className="min-w-fit">
-      {/* Sidebar backdrop (mobile only) */}
-      <div
-        className={`fixed inset-0 bg-gray-900/30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
-          sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-        aria-hidden="true"
-      ></div>
 
-      {/* Sidebar */}
-      <div
-        id="sidebar"
-        ref={sidebar}
-        className={`flex lg:flex! flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-[100dvh] overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:w-64! shrink-0 bg-white dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? "translate-x-0" : "-translate-x-64"} ${variant === 'v2' ? 'border-r border-gray-200 dark:border-gray-700/60' : 'rounded-r-2xl shadow-xs'}`}
-      >
+      
+      {/* Sidebar backdrop (mobile only) */}
+      <div className={`fixed inset-0 bg-gray-900/30 z-40 lg:hidden transition-opacity duration-200 ${sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`} aria-hidden="true"></div>
+
+        {/* Sidebar */}
+        <div
+          ref={sidebar}
+          className={`fixed lg:static flex flex-col left-0 top-0 h-full w-64 lg:w-20 lg:sidebar-expanded:w-64 bg-white dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out ${
+            sidebarOpen ? "translate-x-0" : "-translate-x-64"
+          } lg:translate-x-0`}
+        >
         {/* Sidebar header */}
         <div className="flex justify-between mb-10 pr-3 sm:px-2">
           {/* Close button */}
@@ -366,18 +364,6 @@ function Sidebar({
         </div>
 
         {/* Expand / collapse button */}
-        {/* <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
-          <div className="w-12 pl-4 pr-3 py-2">
-            <button className="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400" onClick={() => setSidebarExpanded(!sidebarExpanded)}>
-              <span className="sr-only">Expand / collapse sidebar</span>
-              <svg className="shrink-0 fill-current text-gray-400 dark:text-gray-500 sidebar-expanded:rotate-180" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                <path d="M15 16a1 1 0 0 1-1-1V1a1 1 0 1 1 2 0v14a1 1 0 0 1-1 1ZM8.586 7H1a1 1 0 1 0 0 2h7.586l-2.793 2.793a1 1 0 1 0 1.414 1.414l4.5-4.5A.997.997 0 0 0 12 8.01M11.924 7.617a.997.997 0 0 0-.217-.324l-4.5-4.5a1 1 0 0 0-1.414 1.414L8.586 7M12 7.99a.996.996 0 0 0-.076-.373Z" />
-              </svg>
-            </button>
-          </div>
-        </div> */}
-
-        {/* Expand / collapse button */}
         <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
           <div className="w-12 pl-4 pr-3 py-2">
             <button
@@ -397,6 +383,8 @@ function Sidebar({
             </button>
           </div>
         </div>
+
+        
       </div>
     </div>
   );
